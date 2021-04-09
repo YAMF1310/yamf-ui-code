6403c32579e246add4eae72426ba40df3da088ad
YAMF <1072664626@qq.com>
2021-04-09 20:29:23 +0800

    动画流畅了

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,9 +1,9 @@
 import Topnav from '@/components/Topnav.vue';
 <template>
-  <div>
-    <Topnav />
+  <div class="layout">
+    <Topnav class="nav" />
     <div class="content">
-      <aside>
+      <aside v-if="menuVisible">
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -20,16 +20,70 @@ import Topnav from '@/components/Topnav.vue';
           </li>
         </ol>
       </aside>
-      <main>主要内容</main>
+      <main><router-view /></main>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { inject, Ref } from "vue";
 import Topnav from "../components/Topnav.vue";
 export default {
   components: { Topnav },
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    return { menuVisible };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+  main {
+    overflow: auto;
+  }
+}
+</style>
