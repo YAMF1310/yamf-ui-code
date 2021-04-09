c871802dc7dde555f86f4254c3019721ebbf7d17
YAMF <1072664626@qq.com>
2021-04-09 21:17:25 +0800

    完成了

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,11 +1,16 @@
 <template>
-  <div><Switch /></div>
+  <div><Switch v-model:value="bool" /></div>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
 export default {
   components: { Switch },
+  setup() {
+    const bool = ref(false);
+    return { bool };
+  },
 };
 </script>
 
